b91b2802b2fc2781424b62aa1da8d5ce67c975b7
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-28 14:50:17 +0530

    displayed book details

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,11 +1,16 @@
 const Book = (props) => {
   return (
     <div>
-      <img src={props.cover_image} alt="" width="200px" height="200px" />
+      <img
+        src={props.bookDetails.cover_image}
+        alt=""
+        width="200px"
+        height="200px"
+      />
       <div>
-        <h2>{props.title}</h2>
-        <p>{props.author}</p>
-        <p>{props.description}</p>
+        <h2>{props.bookDetails.title}</h2>
+        <p>{props.bookDetails.author}</p>
+        <p>{props.bookDetails.description}</p>
       </div>
     </div>
   );
