19ddfdab3dfec36c049757ac75c3bff3b722d5dd
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-28 22:31:28 +0530

    imported style.css and added classname

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,13 +1,14 @@
 import Book from "./Book";
+import "./style.css";
 
 const BookList = (props) => {
   return (
-    <>
+    <div className="book-list">
       {console.log(props)}
       {props.booksData.map((data) => (
         <Book bookDetails={data} key={data.id} />
       ))}
-    </>
+    </div>
   );
 };
 
