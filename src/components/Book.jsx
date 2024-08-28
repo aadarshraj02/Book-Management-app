8c7730f6aa3d4d08411ebc957f42e72098d47747
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-28 14:47:12 +0530

    displayed coverImage,title,author and desc of book

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,5 +1,14 @@
-const Book = () => {
-  return <div></div>;
+const Book = (props) => {
+  return (
+    <div>
+      <img src={props.cover_image} alt="" width="200px" height="200px" />
+      <div>
+        <h2>{props.title}</h2>
+        <p>{props.author}</p>
+        <p>{props.description}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Book;
