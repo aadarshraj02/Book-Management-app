6cd1cee8791b07e25fb5b7c4cb74742800ccb8b4
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-28 23:22:09 +0530

    added function to handle search

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,17 @@ import { Books } from "./utils/mockData";
 import "../src/components/style.css";
 
 const App = () => {
+  const handleSearch = () => {};
+
   return (
     <div>
       <div className="search">
         <h2 className="search-header">Search Books</h2>
-        <input type="text" className="search-input" />
+        <input
+          type="text"
+          className="search-input"
+          onChange={() => handleSearch()}
+        />
         <button className="search-btn">Search</button>
       </div>
       <BookList booksData={Books} />
