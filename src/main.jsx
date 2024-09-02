2381e7ca2221f54a18e687b4b9904cfc3c07b62a
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-02 09:58:14 +0530

    imported and added routes fot About page

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,16 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 // import './index.css'
 import { createBrowserRouter } from "react-router-dom";
+import About from "./components/About.jsx";
 
 const appRouter = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/about",
+    element: <About />,
   },
 ]);
 
