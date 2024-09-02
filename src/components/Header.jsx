26ed9fb2ae6aa2cab30717104eb6ba7836dc2d92
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-02 20:25:03 +0530

    added link for contact and about

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,13 @@
+import { Link } from "react-router-dom";
 import "./Header.css";
 
 const Header = () => {
   return (
     <div>
       <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/about"}>About</Link>
+        <Link to={"/contact"}>Contact</Link>
       </ul>
     </div>
   );
